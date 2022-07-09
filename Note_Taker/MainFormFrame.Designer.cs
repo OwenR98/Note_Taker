7101680a7ee02765e400c296b1d0b344635f1fb8
Owen <owen.ramsey1998@gmail.com>
2022-07-10 00:10:49 +0100

    Add files via upload
    init

---
+++ Note_Taker/MainFormFrame.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Note_Taker
+{
+    partial class MainFormFrame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextArea = new System.Windows.Forms.TextBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextArea
+            // 
+            this.TextArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextArea.Location = new System.Drawing.Point(0, 0);
+            this.TextArea.Multiline = true;
+            this.TextArea.Name = "TextArea";
+            this.TextArea.Size = new System.Drawing.Size(1013, 536);
+            this.TextArea.TabIndex = 0;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.SaveButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.SaveButton.FlatAppearance.BorderSize = 0;
+            this.SaveButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.SaveButton.Location = new System.Drawing.Point(936, 536);
+            this.SaveButton.Margin = new System.Windows.Forms.Padding(0);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "Save File";
+            this.SaveButton.UseVisualStyleBackColor = false;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // MainFormFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(1015, 560);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.TextArea);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "MainFormFrame";
+            this.Text = "Note_Taker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextArea;
+        private System.Windows.Forms.Button SaveButton;
+    }
+}
+
---
+++ Note_Taker/MainFormFrame.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Note_Taker
+{
+    public partial class MainFormFrame : Form
+    {
+        public MainFormFrame()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            
+
+
+            using (StreamWriter sw = File.CreateText(@"C:\Users\OwenR\source\repos\Note_Taker\Files\Test.txt"))
+            {
+                string strVal;
+
+                strVal = TextArea.Text;
+
+                sw.WriteLine(strVal);
+            }
+        }
+    }
+}
