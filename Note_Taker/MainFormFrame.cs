522ec3360e36a4b981b62fdadc13fda9ed1c3ac2
Owen Ramsey <Owen.Ramsey1998@gmail.com>
2022-07-10 20:06:00 +0100

    Sort Usings.

--- Note_Taker/MainFormFrame.cs
+++ Note_Taker/MainFormFrame.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
 
 namespace Note_Taker
