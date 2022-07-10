f9fecd5fef74fbe5d6e6a6914f4b1222879fa7fb
Owen Ramsey <Owen.Ramsey1998@gmail.com>
2022-07-10 19:56:44 +0100

    a

--- Note_Taker/MainFormFrame.cs
+++ Note_Taker/MainFormFrame.cs
@@ -40,7 +40,7 @@ namespace Note_Taker
             // incorrect file path.
             currentDateTime = Regex.Replace(currentDateTime, @"/", "-");
 
-            // Create a new text file and name it using the above logic.a
+            // Create a new text file and name it using the above logic.
             // Then take the content of the main text area and put this into the text file.
             using (StreamWriter sw = File.CreateText(Path.Combine(filePath, currentDateTime)))
             {
